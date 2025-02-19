342e13202acc4a7285214a75990e4ffb22e53d8d
yunsseong <aster20@hanyang.ac.kr>
2025-02-19 14:35:09 +0900

    chore: 초기 프로젝트 설정

---
+++ src/main/java/com/yunsseong/medicine_info_server/MedicineInfoServerApplication.java
@@ -0,0 +1,13 @@
+package com.yunsseong.medicine_info_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicineInfoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicineInfoServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yunsseong/medicine_info_server/MedicineInfoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunsseong.medicine_info_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicineInfoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
